6fecf691660ededac0845c57e581b820409a0c87
aman sagar <amansagar262@gmail.com>
2024-05-30 11:10:41 +0530

    chore: parse env

--- process-env.d.ts
+++ process-env.d.ts
@@ -1,9 +1,11 @@
 import type { InferInput } from 'valibot';
-import { object, string } from 'valibot';
+import { object, string, parse } from 'valibot';
 
 const envVariables = object({
   NODE_ENV: string(),
 });
+
+parse(envVariables, process.env);
 
 declare global {
   namespace NodeJS {
